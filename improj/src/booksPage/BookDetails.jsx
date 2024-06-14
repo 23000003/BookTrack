6a2a4c6923354781154af5a3d5fdc6c31f20e371
Kenny <144243351+23000003@users.noreply.github.com>
2024-06-14 10:08:19 +0800

    Update

--- improj/src/booksPage/BookDetails.jsx
+++ improj/src/booksPage/BookDetails.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function BookDetails(props) {
     const { DisplayDets, book_title, account_name, 
             Location_tag, Location, imagetag, book_author, 
             description, id, in_process, book_price, book_quantity } = props;
 
+    const [Payment, PaymentState] = useState(false);
+    const [top, setTop] = useState('20%');
+    const [height, setHeight] = useState('200px');
+    const [Choose, SetChoose] = useState(true);
+    const [PaymentMethod, SetPaymentMethod] = useState(); //false if gcash, true if COD
+    const [quantity, setQuantity] = useState(1);
+    
+    const QuantityAdd = () =>{
+        setQuantity(quantity => quantity++);
+    }
+
+    const QuantityMinus = () =>{
+        setQuantity(quantity => quantity--);
+    }
+
+    const gcashMethod = () =>{
+        SetPaymentMethod('Gcash');
+        SetChoose(false);
+        setHeight('730px')
+        setTop('33.4%')
+    }
+
+    const CashOnDelivery = () =>{
+        SetPaymentMethod('COD');
+        SetChoose(false);
+        setHeight('370px')
+        setTop('25.4%')
+    }
+
+    const paymentTrigger = () =>{
+        PaymentState(!Payment);
+    }
+
+    const returnBackDisplay = () =>{
+        PaymentState(false);
+        setTop('20%');
+        setHeight('200px');
+        SetChoose(true);
+        SetPaymentMethod();
+    }
+
     return (
+        <>
         <div className="BookDetailsContainer">
             <hr style={{ margin: '0px 10%' }} />
             <div className="Book-Details">
@@ -32,11 +74,11 @@ export default function BookDetails(props) {
                             <h2>₱{book_price}.00</h2>
                             <div className="Qty">
                                 <p>Qty: {book_quantity}</p>
-                                <button className="qty" style={{ borderRight: '0px', cursor: 'pointer' }}>-</button>
-                                <span>1</span>
-                                <button className="qty" style={{ borderLeft: '0px', cursor: 'pointer' }}>+</button>
+                                <button className="qty" style={{ borderRight: '0px', cursor: 'pointer' }} onClick={() => QuantityMinus()}>-</button>
+                                <span>{quantity}</span>
+                                <button className="qty" style={{ borderLeft: '0px', cursor: 'pointer' }} onClick={() => QuantityAdd()}>+</button>
                                 <br />
-                                <button className="Buy">Buy Now</button>
+                                <button className="Buy" onClick={() => paymentTrigger()}>Buy Now</button>
                             </div>
                         </div>
                         <hr style={{ marginTop: '25px' }} />
@@ -55,5 +97,81 @@ export default function BookDetails(props) {
                 </div>
             </div>
         </div>
+        
+        {Payment && 
+        <>
+            <div className="Buy-Display1" onClick={() => returnBackDisplay()}></div>
+            <div className="Buy-Display" style={{height: height, top: top}}>
+                
+                {Choose &&
+                    <div className="Payment-Contents">
+                        <h3>Select Payment Method</h3>
+                        <hr/>
+                        <div className="Methods">
+                            <div className="Gcash" onClick={() => gcashMethod()}> {/**onclick="gcashMethod()" */}
+                                <p>Gcash</p>
+                            </div>
+                            <div className="COD" onClick={() => CashOnDelivery()}> {/**onclick="codMethod()" */}
+                                <p>Cash on Delivery</p>
+                            </div>
+                        </div>
+                    </div>
+                }
+
+                {PaymentMethod === 'Gcash' && (
+                    <div className="gcash-content">
+                        <h3 style={{marginTop: "25px"}}>LIBRARY GCASH DETAILS</h3>
+                        <div>
+                            <img src="https://wfiljmekszmbpzaqaxys.supabase.co/storage/v1/object/public/images/GCash.png?t=2024-05-29T10%3A48%3A45.460Z" alt="" style={{maxWidth: "50%", maxHeight: "50%", objectFit: "cover"}}/>
+                        </div>
+                        <p id="phonenumber">099-5281-3643</p>
+                        <div className="amount-paying">
+                            <p className="paying-amount"></p>
+                            <p id="delivery-fee" style={{marginLeft: "15px", display: "none"}}>+ 50 Delivery Fee</p>
+                        </div>
+                        <div className="choices-type">
+                            <input type="checkbox" id="pickup-choice"/><label>Pick up</label> {/**onclick="pickup()" */}
+                            <input type="checkbox" id="delivery-choice" style={{marginLeft: "55px"}}/><label>Delivery</label> {/* onclick="delivery()"* */}
+                        </div>
+                        <div className="gcash-text">
+                            <input type="text" placeholder="Input Reference Number"/>
+                            <div className="gcash-text2">
+                                <input type="text" style={{marginRight: "5px"}} placeholder="First Name"/>
+                                <input type="text" style={{marginRight: "5px"}} placeholder="Last Name"/>
+                            </div>
+                            <input type="text" placeholder="Contact Number"/>
+                            <input type="text" id="delivery-location" placeholder="Location" disabled/>
+                            <div>
+                                <button id="BuyItem1">Done</button> {/**onclick="buyitem() */}
+                            </div>
+                        </div>
+                    </div>
+                )}
+                
+                {PaymentMethod === 'COD' &&(
+                    <div className="cod-content">
+                        <h3 style={{marginTop: "35px"}}>CASH ON DELIVERY</h3>
+                        <div className="cod-text">
+                            <div className="amount-paying">
+                                <p className="paying-amount"></p>
+                                <p style={{marginLeft: "15px"}}>+ 50 Delivery Fee</p>
+                            </div>
+                            <div className="cod-text2">
+                                <input type="text" style={{marginRight: "5px"}} placeholder="First Name"/>
+                                <input type="text" style={{marginRight: "5px"}}  placeholder="Last Name"/>
+                            </div>
+                            <input type="text" placeholder="Contact Number"/>
+                            <input type="text" placeholder="Location"/>
+                            <div>
+                                <button id="BuyItem2">Done</button> {/**onclick="buyitem() */}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+            </>
+            }
+
+        </>
     );
 }
