30f39d16d106d7fdb34a681fa628f9c45f4951a7
Kenny <144243351+23000003@users.noreply.github.com>
2024-06-20 20:01:39 +0800

    w

--- improj/src/Supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://wfiljmekszmbpzaqaxys.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmaWxqbWVrc3ptYnB6YXFheHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3NTQ1MTksImV4cCI6MjAyOTMzMDUxOX0.t5GOLxJHHdEAQytpgduJACIEt6HyTydp6v_1tmBDrhA';
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
